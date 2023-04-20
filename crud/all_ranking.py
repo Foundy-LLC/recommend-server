7e95e5876e5dcf2a638694cf447592e077cdb4cd
jungsiroo <fordevelop@pm.me>
2023-04-20 14:58:33 +0900

    [CFRS151 - GET Ranking] rank over() 를 활용하여 랭킹 재정렬

--- crud/all_ranking.py
+++ crud/all_ranking.py
@@ -6,14 +6,13 @@ LIMIT = 50
 
 def get_all_ranking(db: Session, weekly: bool, page: int):
     criteria_score = "weekly_score" if weekly else "total_score"
-    criteria_rank = "weekly_ranking" if weekly else "total_ranking"
 
     query = f"""
     WITH
-        rank_result AS (select user_id as user_id, {criteria_score} as score, {criteria_rank} as rank from user_ranking)
+        rank_result AS (select user_id as user_id, {criteria_score} as score from user_ranking)
 
     select id, name, profile_image as profileImage, ranking.score as rankingScore, \
-    ranking.rank as ranking, status
+    rank() over (order by ranking.score desc) as ranking, status
     from user_account
     join (
         table rank_result
@@ -40,15 +39,14 @@ def get_org_ranking(db: Session, organizationId: int, page: int):
         return ["NO ORG"]
 
     query = f"""
-    select total.id, name, profileImage, rankingScore, ranking, status
+    select total.id, name, total.profileImage, total.rankingScore, \
+    rank() over (order by total.rankingScore desc) as ranking, total.status
     from
     (
-        select id, name, profile_image as profileImage, ranking.total_score as rankingScore, \
-        ranking.total_ranking as ranking, status
+        select id, name, profile_image as profileImage, ranking.total_score as rankingScore, status
         from user_account
         join (
-            select user_id, total_score, total_ranking from user_ranking
-            order by total_ranking
+            select user_id, total_score from user_ranking
         ) as ranking
         on ranking.user_id = user_account.id
     ) as total
