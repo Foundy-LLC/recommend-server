83683559becef43b838eaf89c60bf37f08ba16a6
jungsiroo <sh2298@naver.com>
2023-05-06 13:19:30 +0900

    [CFRS-251] Room score 를 계산한다. equation 은 아래 설명 참조.

---
+++ crud/room_ranking.py
@@ -0,0 +1,49 @@
+import math
+
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+BASE = 6
+
+
+def get_active_secs_query(room_id, count):
+    query = f"""
+    select duration_sec from room_ignition
+    where room_id = '{room_id}' 
+    and user_count={count}
+    """
+
+    return query
+
+
+def get_update_ranking_query(room_id, score):
+    query = f"""
+    update room
+    set ranking_score = {score}
+    where id='{room_id}'
+    """
+
+    return query
+
+
+def calc_room_rating(active_secs, count):
+    return (count - 1) * pow(1.2, count - 1) * math.log(active_secs, BASE - count)
+
+
+def update_room_ranking(db: Session):
+    rooms_q = db.execute(text('select room_id, user_count from room_ignition'))
+
+    room_scores = {}
+
+    for room_id, cnt in rooms_q:
+        query = get_active_secs_query(room_id, cnt)
+        active_secs = db.execute(text(query)).scalar()
+
+        room_scores[room_id] = room_scores.get(room_id, 0) + calc_room_rating(active_secs, cnt)
+
+    for room_id, score in room_scores.items():
+        query = get_update_ranking_query(room_id, score)
+        db.execute(text(query))
+        db.commit()
+
+    print("ROOM RATING UPDATE COMPLETE")
